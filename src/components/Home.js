1ecec6ed4e15609de7c06d20763de5029f6d35b5
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-07-29 15:02:17 +0300

    Project

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,25 +6,25 @@ function Home() {
       <div style={{background: "coral", color: "white", marginLeft: "300px", marginRight: "300px"}}>
           <h1>Welcome to This FruWeb Website, the Home of Fruits!</h1>
           <div style={{background: "blue", color: "white", marginLeft: "100px", marginRight: "100px"}}>
-          <p>
-              <i>Are you passionate about fruits?</i>
-          </p>
-          <p>
-              <i>Do you want to select and know more about your favorite fruits?</i>
-          </p>
-          <p>
-              <i>Do you know more about the content of a fruit?</i>
-          </p>
-          
-          <p>
-              <i>Do you want to add a fruit that is not in the list?</i>
-          </p>
-          <p>
-              <i>Do you want to search for a fruit and know whether it is in the list?</i>
-          </p>
-          <p>
-              <i>Do you want to order fruits in the list to help you make a decision on which one to buy based on your health status?</i>
-          </p><br></br>
+              <p>
+                  <i>Are you passionate about fruits?</i>
+              </p>
+              <p>
+                  <i>Do you want to select and know more about your favorite fruits?</i>
+              </p>
+              <p>
+                  <i>Do you know more about the content of a fruit?</i>
+              </p>
+            
+              <p>
+                  <i>Do you want to add a fruit that is not in the list?</i>
+              </p>
+              <p>
+                  <i>Do you want to search for a fruit and know whether it is in the list?</i>
+              </p>
+              <p>
+                  <i>Do you want to order fruits in the list to help you make a decision on which one to buy based on your health status?</i>
+              </p><br></br>
           </div>
 
           <p>
@@ -32,19 +32,19 @@ function Home() {
           </p>
 
           <div style={{background: "indigo", color: "white"}}>
-          <p>
-              <em>You can click the <Link to="/about">About</Link> button to know more about this website.</em>
-          </p>
-          <p>
-              <em>You can click the <Link to="/fruitContainer">Fruits</Link> button to be able to perform some actions on the fruits.</em>
-          </p>
-          <p>
-              <em>Click the <Link to="/favoriteFruitsContainer">Favorites</Link> button to be able to select your favorite fruits and see them by clicking Show/hide Favorite Fruits button.</em>
-          </p>
+              <p>
+                    <em>You can click the <Link to="/about">About</Link> button to know more about this website.</em>
+              </p>
+              <p>
+                    <em>You can click the <Link to="/fruitContainer">Fruits</Link> button to be able to perform some actions on the fruits.</em>
+                </p>
+                <p>
+                    <em>Click the <Link to="/favoriteFruitsContainer">Favorites</Link> button to be able to select your favorite fruits and see them by clicking Show/hide Favorite Fruits button.</em>
+                </p>
           </div>
           
       </div>
-  )
+  );
   
   
 }
