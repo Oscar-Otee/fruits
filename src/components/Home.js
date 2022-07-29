f5c3b475ac2730d6084aa01064975d3434ebe0ac
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-07-29 13:36:18 +0300

    Project

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,14 @@ function Home() {
   return (
       <div style={{background: "coral", color: "white"}}>
           <h1>Welcome to This FruWeb Website, the Home of Fruits!</h1>
-          <h4>
-              You can click the <Link to="/about">About</Link> button to know more about this website.
-          </h4>
           <p>
-          You can also click the <Link to="/fruitContainer">Fruits</Link> button to be able to perform some actions on the fruits.
+              You can click the <Link to="/about">About</Link> button to know more about this website.
+          </p>
+          <p>
+          You can click the <Link to="/fruitContainer">Fruits</Link> button to be able to perform some actions on the fruits.
+          </p>
+          <p>
+          Click the <Link to="/favoriteFruitsContainer">Favorites</Link> button to be able to select your favorite fruits and see them by clicking Show/hide Favorite Fruits button.
           </p>
       </div>
   )
