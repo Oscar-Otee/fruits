de46e630dc3911f901684f768b0fdd7627748ee8
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-07-29 14:41:40 +0300

    Project

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,22 +23,26 @@ function Home() {
               <i>Do you want to search for a fruit and know whether it is in the list?</i>
           </p>
           <p>
-              <i>Do you want to order fruits in the in the list to help you make a decision on which one to buy based on your health status?</i>
+              <i>Do you want to order fruits in the list to help you make a decision on which one to buy based on your health status?</i>
           </p><br></br>
           </div>
 
           <p>
               <strong style={{color: "darkcyan"}}>FruWeb is the answer to the above questions. So welcome and feel free to navigate the site by cliking the bellow capitalized words marked in bue or the buttons with green color above.</strong>
           </p>
+
+          <div>
           <p>
-              You can click the <Link to="/about">About</Link> button to know more about this website.
+              <em>You can click the <Link to="/about">About</Link> button to know more about this website.</em>
           </p>
           <p>
-              You can click the <Link to="/fruitContainer">Fruits</Link> button to be able to perform some actions on the fruits.
+              <em>You can click the <Link to="/fruitContainer">Fruits</Link> button to be able to perform some actions on the fruits.</em>
           </p>
           <p>
-              Click the <Link to="/favoriteFruitsContainer">Favorites</Link> button to be able to select your favorite fruits and see them by clicking Show/hide Favorite Fruits button.
+              <em>Click the <Link to="/favoriteFruitsContainer">Favorites</Link> button to be able to select your favorite fruits and see them by clicking Show/hide Favorite Fruits button.</em>
           </p>
+          </div>
+          
       </div>
   )
   
