41c87e1176ade2423cbe5bfb020c3f579506855f
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-07-29 12:59:13 +0300

    Project

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 function Home() {
   return (
       <div style={{background: "coral", color: "white"}}>
           <h1>Welcome to This FruWeb Website, the Home of Fruits!</h1>
           <h4>
-              You can click the About Button to know more about this website.
+              You can click the <Link to="/about">About</Link> button to know more about this website.
           </h4>
+          <p>
+          You can also click the <Link to="/fruitContainer">Fruits</Link> button to be able to perform some actions on the fruits.
+          </p>
       </div>
   )
   
