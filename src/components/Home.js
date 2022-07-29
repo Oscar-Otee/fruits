1fcdf2eda14d4fdd063c52aae197e744fdf00ae2
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-07-29 14:14:20 +0300

    Project

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,27 +6,27 @@ function Home() {
       <div style={{background: "coral", color: "white"}}>
           <h1>Welcome to This FruWeb Website, the Home of Fruits!</h1>
           <p>
-              Are you passionate about fruits?
+              <i>Are you passionate about fruits?</i>
           </p>
           <p>
-              Do you want to select your favorite fruits?
+              <i>Do you want to select your favorite fruits?</i>
           </p>
           <p>
-              Do you know more about the content of a fruit?
+              <i>Do you know more about the content of a fruit?</i>
           </p>
           
           <p>
-              Do you want to add a fruit that is not in the list?
+              <i>Do you want to add a fruit that is not in the list?</i>
           </p>
           <p>
-              Do you want to search for a fruit and know whether it is in the list?
+              <i>Do you want to search for a fruit and know whether it is in the list?</i>
           </p>
           <p>
-              Do you want to order fruits in the in the list to help you make a decision on which one to buy based on your health status?
+              <i>Do you want to order fruits in the in the list to help you make a decision on which one to buy based on your health status?</i>
           </p><br></br>
 
           <p>
-              <strong>FruWeb is the answer to the above questions. So welcome and feel free to navigate the site by cliking the bellow words marked in bue.</strong>
+              <strong style={{color: "darkcyan"}}>FruWeb is the answer to the above questions. So welcome and feel free to navigate the site by cliking the bellow capitalized words marked in bue or the buttons with green color above.</strong>
           </p>
           <p>
               You can click the <Link to="/about">About</Link> button to know more about this website.
