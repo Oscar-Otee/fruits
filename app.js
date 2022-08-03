4691c2cf6b19a60a0711dc8bb6ad9b83d55b7cbb
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-08-03 19:49:27 +0300

    project

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000;
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+        req.sendFile(path.resolve(__dirname, "build", "index.html"));
+    })
+}
+app.listen(port, (err) => {
+    if (err) return console.log(err);
+    console.log("Server running on port: ", port)
+})
+
+// const server = jsonServer.create();
+// const router = jsonServer.router("./db.json");
+// const middlewares = jsonserver.defaults({
+//     stactic: ".build"
+// })
+
+// const port = process.env.PORT || 3000;
+// server.use(middlewares);
+// server.use(
+//     jsonServer.rewriter({
+//         "/api/*": "/$1",
+//     })
+// );
+// server.use(router);
+// server.listen(port, () => {
+//     console.log(`Server is running on ${port}`);
+// })
