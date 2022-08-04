98590593abaf86480352bd3bbf298c063f6ecaff
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-08-04 07:53:44 +0300

    project

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const port = process.env.PORT || 3000;
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-        req.sendFile(path.resolve(__dirname, "build", "index.html"));
-    })
-}
-app.listen(port, (err) => {
-    if (err) return console.log(err);
-    console.log("Server running on port: ", port)
-})
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+
+
+// const express = require("express");
+// const app = express();
+// const path = require("path");
+// const port = process.env.PORT || 3000;
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("build"));
+//     app.get("*", (req, res) => {
+//         req.sendFile(path.resolve(__dirname, "build", "index.html"));
+//     })
+// }
+// app.listen(port, (err) => {
+//     if (err) return console.log(err);
+//     console.log("Server running on port: ", port);
+// })
+
+
 
 // const server = jsonServer.create();
 // const router = jsonServer.router("./db.json");
